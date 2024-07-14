22cf9c21c8d5c39efdd01227d6b0ca819a798276
pria-pria <thepriyaduggal@gmail.com>
2024-07-14 17:15:26 +0530

    added files

--- saas_foundation/settings.py
+++ saas_foundation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j&t*xdv-^q#0mp#1tu0owodbu3x#r=j+uyhww_(i&0-zw&8ztc'
+SECRET_KEY = "SEC_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
